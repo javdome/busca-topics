4ed11fd51480bcd74361b41e664bffe441f5121e
Javi Domenech <javdome@gmail.com>
2022-10-03 12:42:02 +0200

    Added Dark mode option

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom"
 
 const Home = ()=> {
 
+  const toggleDarkMode = () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
   return (
     <div className="menu">
         <h1>Buscador Iceri22</h1>
@@ -10,6 +15,8 @@ const Home = ()=> {
         <Link to="/topics">Topics</Link>
         <Link to="/tracks">Tracks</Link>
         {/* <Link to="/products">Products</Link> */}
+
+        <button onClick={toggleDarkMode}>Dark/Light mode</button>
 
     </div>
   )
